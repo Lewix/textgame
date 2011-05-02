5b787a848718a966065b375439cb93a09575a666
Bellerofront <belerofront@gmail.com>
2011-05-02 09:02:30 -0700

    Edited ItemContainer.java via GitHub

--- ItemContainer.java
+++ ItemContainer.java
@@ -3,42 +3,38 @@ package textgame;
 import java.util.*;
 
 public abstract class ItemContainer {
-	private List<Item> itemList;
-
-    /**
-     * Lets you initialise the ItemContainer with an existing List of items.
-     *
-     * @author Colin Rothwell
-     */
-    public ItemContainer(List<Item> itemList) {
-        this.itemList = itemList;
-    }
+	private List<Item> itemList = new ArrayList<Item>();
+	private int id;
 	
-	protected ItemContainer(){
-		itemList =  new ArrayList<Item>();
+	public ItemContainer(int i){
+		id = i;
 	}
 	
 	//add a newly created item in the container 
-	protected void add(Item it){
+	public void addItem(Item it){
 		itemList.add(it);
 	}
 	
 	//removes the item from the container 
-	protected void remove(Item it){
+	public void removeItem(Item it){
 		itemList.remove(it);
 	}
 	
-	protected boolean contains(Item it){
+	public boolean containsItem(Item it){
 		return itemList.contains(it);
 	}
 	
-	protected Item contains(String s){
+	public Item contains(String s){
 		for (Item it : itemList) {
 			//if list contain an item with name equal to s return true
 			if (it.getName().equals(s)) return it;
 		}
 		//if list does not contain an item with name equal to s return false
 		return null;
+	}
+	
+	public int getId(){
+		return id;
 	}
 	
 }
